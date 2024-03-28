35cf079bb8ab29ab15eabb7a8028eab8fd9d5db8
Mecoli <mecoli@Mecoli-MacBook-Pro.local>
2024-03-28 17:13:44 +0800

    hw4 done

--- BLE_GattServer_AddService/source/main.cpp
+++ BLE_GattServer_AddService/source/main.cpp
@@ -18,24 +18,29 @@
 #include "ble/BLE.h"
 #include "ble/gap/Gap.h"
 #include "ble/services/HeartRateService.h"
+#include "ButtonService.h"
 #include "pretty_printer.h"
 #include "mbed-trace/mbed_trace.h"
 
 using namespace std::literals::chrono_literals;
 
-const static char DEVICE_NAME[] = "Heartrate";
+const static char DEVICE_NAME[] = "Group1";
 
 static events::EventQueue event_queue(/* event count */ 16 * EVENTS_EVENT_SIZE);
 
-class HeartrateDemo : ble::Gap::EventHandler {
+class HeartrateDemo : ble::Gap::EventHandler
+{
 public:
-    HeartrateDemo(BLE &ble, events::EventQueue &event_queue) :
-        _ble(ble),
-        _event_queue(event_queue),
-        _heartrate_uuid(GattService::UUID_HEART_RATE_SERVICE),
-        _heartrate_value(100),
-        _heartrate_service(ble, _heartrate_value, HeartRateService::LOCATION_FINGER),
-        _adv_data_builder(_adv_buffer)
+    HeartrateDemo(BLE &ble, events::EventQueue &event_queue) : _ble(ble),
+                                                               _event_queue(event_queue),
+                                                               _led1(LED1, 1),
+                                                               _button(BLE_BUTTON_PIN_NAME, BLE_BUTTON_PIN_PULL),
+                                                               _button_service(NULL),
+                                                               _button_uuid(ButtonService::BUTTON_SERVICE_UUID),
+                                                               _heartrate_uuid(GattService::UUID_HEART_RATE_SERVICE),
+                                                               _heartrate_value(100),
+                                                               _heartrate_service(ble, _heartrate_value, HeartRateService::LOCATION_FINGER),
+                                                               _adv_data_builder(_adv_buffer)
     {
     }
 
@@ -43,6 +48,7 @@ public:
     {
         _ble.init(this, &HeartrateDemo::on_init_complete);
 
+        _event_queue.call_every(500, this, &HeartrateDemo::blink);
         _event_queue.dispatch_forever();
     }
 
@@ -50,7 +56,8 @@ private:
     /** Callback triggered when the ble initialization process has finished */
     void on_init_complete(BLE::InitializationCompleteCallbackContext *params)
     {
-        if (params->error != BLE_ERROR_NONE) {
+        if (params->error != BLE_ERROR_NONE)
+        {
             printf("Ble initialization failed.");
             return;
         }
@@ -63,10 +70,15 @@ private:
         /* heart rate value updated every second */
         _event_queue.call_every(
             1000ms,
-            [this] {
+            [this]
+            {
                 update_sensor_value();
-            }
-        );
+            });
+
+        _button_service = new ButtonService(_ble, false /* initial value for button pressed */);
+
+        _button.fall(Callback<void()>(this, &HeartrateDemo::button_pressed));
+        _button.rise(Callback<void()>(this, &HeartrateDemo::button_released));
 
         start_advertising();
     }
@@ -77,32 +89,32 @@ private:
 
         ble::AdvertisingParameters adv_parameters(
             ble::advertising_type_t::CONNECTABLE_UNDIRECTED,
-            ble::adv_interval_t(ble::millisecond_t(100))
-        );
+            ble::adv_interval_t(ble::millisecond_t(100)));
 
         _adv_data_builder.setFlags();
         _adv_data_builder.setAppearance(ble::adv_data_appearance_t::GENERIC_HEART_RATE_SENSOR);
         _adv_data_builder.setLocalServiceList({&_heartrate_uuid, 1});
+        _adv_data_builder.setLocalServiceList(mbed::make_Span(&_button_uuid, 1));
         _adv_data_builder.setName(DEVICE_NAME);
 
         /* Setup advertising */
 
         ble_error_t error = _ble.gap().setAdvertisingParameters(
             ble::LEGACY_ADVERTISING_HANDLE,
-            adv_parameters
-        );
+            adv_parameters);
 
-        if (error) {
+        if (error)
+        {
             printf("_ble.gap().setAdvertisingParameters() failed\r\n");
             return;
         }
 
         error = _ble.gap().setAdvertisingPayload(
             ble::LEGACY_ADVERTISING_HANDLE,
-            _adv_data_builder.getAdvertisingData()
-        );
+            _adv_data_builder.getAdvertisingData());
 
-        if (error) {
+        if (error)
+        {
             printf("_ble.gap().setAdvertisingPayload() failed\r\n");
             return;
         }
@@ -111,7 +123,8 @@ private:
 
         error = _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
 
-        if (error) {
+        if (error)
+        {
             printf("_ble.gap().startAdvertising() failed\r\n");
             return;
         }
@@ -125,7 +138,8 @@ private:
         _heartrate_value++;
 
         /*  60 <= bpm value < 110 */
-        if (_heartrate_value == 110) {
+        if (_heartrate_value == 110)
+        {
             _heartrate_value = 60;
         }
 
@@ -137,7 +151,8 @@ private:
     /* when we connect we stop advertising, restart advertising so others can connect */
     virtual void onConnectionComplete(const ble::ConnectionCompleteEvent &event)
     {
-        if (event.getStatus() == ble_error_t::BLE_ERROR_NONE) {
+        if (event.getStatus() == ble_error_t::BLE_ERROR_NONE)
+        {
             printf("Client connected, you may now subscribe to updates\r\n");
         }
     }
@@ -149,10 +164,26 @@ private:
 
         ble_error_t error = _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
 
-        if (error) {
+        if (error)
+        {
             printf("_ble.gap().startAdvertising() failed\r\n");
             return;
         }
+    }
+
+    void button_pressed(void)
+    {
+        _event_queue.call(Callback<void(bool)>(_button_service, &ButtonService::updateButtonState), true);
+    }
+
+    void button_released(void)
+    {
+        _event_queue.call(Callback<void(bool)>(_button_service, &ButtonService::updateButtonState), false);
+    }
+
+    void blink(void)
+    {
+        _led1 = !_led1;
     }
 
 private:
@@ -162,6 +193,11 @@ private:
     UUID _heartrate_uuid;
     uint16_t _heartrate_value;
     HeartRateService _heartrate_service;
+
+    DigitalOut _led1;
+    InterruptIn _button;
+    ButtonService *_button_service;
+    UUID _button_uuid;
 
     uint8_t _adv_buffer[ble::LEGACY_ADVERTISING_MAX_SIZE];
     ble::AdvertisingDataBuilder _adv_data_builder;
@@ -185,4 +221,3 @@ int main()
 
     return 0;
 }
-
